78a8c1e65b38f446d1521af29f72b87256eddecd
shufol islam <shufolislam1255@gmail.com>
2022-12-27 23:38:06 +0600

    setup all done for client side

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App

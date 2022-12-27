b60f29820ff3fa0e90cb58e9aa503ac06d2726e1
shufol islam <shufolislam1255@gmail.com>
2022-12-28 00:39:06 +0600

    home page upper part done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,24 +3,36 @@ import Typewriter from 'typewriter-effect';
 
 const Home = () => {
     return (
-        <div className='pb-60'>
-            <div className='flex justify-between grid grid-cols-2 gap-16 '>
-                <div className='pt-16 text-xl font-semibold whitespace-nowrap text-left ml-12'>
-                    <p className='text-2xl text-blue-600'>For Your Dream</p>
-                    <p>
-                        <Typewriter
-                            options={{
-                                strings: ['WE ARE HERE', 'WITH YOU', 'ALWAYS'],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-                    </p>
-                </div>
-                <div>
-                    <img src="https://i.ibb.co/5XDK7m4/29493.jpg" alt="not" srcset="" />
-                </div>
+        // <div className='pb-60'>
+        //     <div className='flex justify-between grid grid-cols-2 gap-16 '>
+        //         <div className='pt-16 text-xl font-semibold whitespace-nowrap text-left ml-12'>
+        //             <p className='text-2xl text-blue-600'>For Your Dream</p>
+        //             <p>
 
+        //             </p>
+        //         </div>
+        //         <div>
+        //             <img src="https://i.ibb.co/5XDK7m4/29493.jpg" alt="not" srcset="" />
+        //         </div>
+
+        //     </div>
+        // </div>
+        <div className="flex gap-16 pb-60">
+            <div className="w-1/2 p-4 text-left pt-8 ml-12">
+            <p className='text-2xl text-blue-600 font-bold'>For Your Dream</p>
+                <p className="text-xl font-bold">
+                    <Typewriter
+                        options={{
+                            strings: ['WE ARE HERE', 'WITH YOU', 'ALWAYS'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                </p>
+                <p className='mt-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident rerum nisi obcaecati. Nostrum voluptas recusandae, modi culpa neque laudantium sunt qui fugit facilis mollitia maiores facere iure. Natus, assumenda rem!</p>
+            </div>
+            <div className="w-1/2">
+                <img src="https://i.ibb.co/5XDK7m4/29493.jpg" className="rounded-lg" />
             </div>
         </div>
     );
